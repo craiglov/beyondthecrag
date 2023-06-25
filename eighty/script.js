07035e473e37ce6c3e83ad7b9b906bba34d90e2d
anh0034 <hayandrew2@gmail.com>
2023-06-25 02:11:28 -0400

    lots of bracelet

---
+++ eighty/script.js
@@ -0,0 +1,115 @@
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all the grid items
+    var gridItems = document.querySelectorAll('.grid-item');
+  
+    // Define the array of image sources
+    var images = [
+      'beads/lightblue.png',
+      'beads/white.png',
+      'beads/lightpink.png',
+      'beads/lightorange.png',
+      'beads/lightgreen.png',
+      'beads/a.png',
+      'beads/b.png',
+      'beads/c.png',
+      'beads/d.png',
+      'beads/e.png',
+      'beads/f.png',
+      'beads/g.png',
+      'beads/h.png',
+      'beads/i.png',
+      'beads/j.png',
+      'beads/k.png',
+      'beads/l.png',
+      'beads/m.png',
+      'beads/n.png',
+      'beads/o.png',
+      'beads/p.png',
+      'beads/q.png',
+      'beads/r.png',
+      'beads/s.png',
+      'beads/t.png',
+      'beads/u.png',
+      'beads/v.png',
+      'beads/w.png',
+      'beads/x.png',
+      'beads/y.png',
+      'beads/z.png'
+    ];
+  
+    // Create the show/hide button element
+    var toggleButton = document.createElement('button');
+    toggleButton.className = 'toggle-button';
+    toggleButton.innerHTML = 'Show Arrows';
+    toggleButton.style.display = 'none';
+  
+    // Append the toggle button to the body
+    document.body.appendChild(toggleButton);
+  
+    // Add click event listener to the toggle button
+    toggleButton.addEventListener('click', function() {
+      var arrows = document.querySelectorAll('.arrow');
+      if (arrows[0].style.display === 'none') {
+        arrows.forEach(function(arrow) {
+          arrow.style.display = 'block';
+        });
+        toggleButton.innerHTML = 'Hide Arrows';
+      } else {
+        arrows.forEach(function(arrow) {
+          arrow.style.display = 'none';
+        });
+        toggleButton.innerHTML = 'Show Arrows';
+      }
+    });
+  
+    // Add click event listeners to the arrow elements
+    gridItems.forEach(function(item) {
+      // Set the initial index to 0
+      item.querySelector('img').setAttribute('data-index', 0);
+  
+      // Create the up arrow element
+      var upArrow = document.createElement('div');
+      upArrow.className = 'arrow up-arrow';
+      upArrow.innerHTML = '&#8593;';
+  
+      // Create the down arrow element
+      var downArrow = document.createElement('div');
+      downArrow.className = 'arrow down-arrow';
+      downArrow.innerHTML = '&#8595;';
+  
+      // Append the arrows to the grid item
+      item.appendChild(upArrow);
+      item.appendChild(downArrow);
+  
+      // Hide the arrows by default
+      upArrow.style.display = 'none';
+      downArrow.style.display = 'none';
+  
+      // Add click event listener to the up arrow
+      upArrow.addEventListener('click', function(event) {
+        event.stopPropagation();
+        var img = item.querySelector('img');
+        var currentIndex = parseInt(img.getAttribute('data-index'));
+        var previousIndex = (currentIndex - 1 + images.length) % images.length;
+        img.src = images[previousIndex];
+        img.setAttribute('data-index', previousIndex);
+      });
+  
+      // Add click event listener to the down arrow
+      downArrow.addEventListener('click', function(event) {
+        event.stopPropagation();
+        var img = item.querySelector('img');
+        var currentIndex = parseInt(img.getAttribute('data-index'));
+        var nextIndex = (currentIndex + 1) % images.length;
+        img.src = images[nextIndex];
+        img.setAttribute('data-index', nextIndex);
+      });
+    });
+  
+    // Show the toggle button after a delay
+    setTimeout(function() {
+      toggleButton.style.display = 'block';
+    }, 1000);
+  });
+  

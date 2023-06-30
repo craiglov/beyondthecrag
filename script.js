84a5a8a0d66c70c9e7f6b1c8cff3b0beb0705644
anh0034 <hayandrew2@gmail.com>
2023-06-29 23:11:51 -0400

    script off

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.pageYOffset;
-    var foreground = document.querySelector('.foreground');
-    var background = document.querySelector('.background');
-
-    foreground.style.transform = 'translate3d(0, -' + scrollPosition * 2 + 'px, 0)';
-    background.style.transform = 'translate3d(0, -' + scrollPosition * 1 + 'px, 0)';
-});
